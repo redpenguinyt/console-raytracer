17765ac072ba103eeee2bd2ed55f95828eb0a742
Red Penguin <timmyterskov@gmail.com>
2023-09-13 15:00:18 +0100

    extra comments

--- src/main.rs
+++ src/main.rs
@@ -15,34 +15,34 @@ fn main() {
         // Transform3D::new_tr(Vec3D::new(3.0, 0.0, 0.0), Vec3D::new(0.0, 0.7, 0.0)),
         vec![
             RaySphere::new(
-                Vec3D::new(0.0, -1.0, 3.0),
-                1.0,
-                Colour::rgb(255, 0, 0),
-                500.0,
-                0.2,
-            ), // Red
+                Vec3D::new(0.0, -1.0, 3.0), // Position
+                1.0, // Radius
+                Colour::rgb(255, 0, 0), // Colour (red)
+                500.0, // Specular (in this case, quite shiny)
+                0.2, // Reflectiveness (a value from 0 to 1)
+            ),
             RaySphere::new(
                 Vec3D::new(2.0, 0.0, 4.0),
                 1.0,
-                Colour::rgb(0, 0, 255),
+                Colour::rgb(0, 0, 255), // Blue
                 500.0,
                 0.3,
-            ), // Blue
+            ),
             RaySphere::new(
                 Vec3D::new(-2.0, 0.0, 4.0),
                 1.0,
-                Colour::rgb(0, 255, 0),
-                10.0,
+                Colour::rgb(0, 255, 0), // Green
+                10.0, // somewhat shiny
                 0.4,
-            ), // Green
+            ),
             RaySphere::new(
                 Vec3D::new(0.0, -5001.0, 0.0),
                 5000.0,
-                Colour::rgb(255, 255, 0),
-                1000.0,
+                Colour::rgb(255, 255, 0), // Yellow
+                1000.0, // very shiny
                 0.1,
-            ), // Yellow
-            RaySphere::new(Vec3D::new(0.0, 3.0, 5.0), 2.0, Colour::WHITE, 10.0, 0.7),
+            ),
+            RaySphere::new(Vec3D::new(0.0, 3.0, 5.0), 2.0, Colour::WHITE, 2.0, 0.7),
         ],
         vec![
             Light::new_ambient(0.2),
@@ -51,6 +51,9 @@ fn main() {
         ],
     );
 
+    // Render all the 3D stuff
     canvas.blit(&scene.render(canvas.size()), Wrapping::Panic);
+
+    // Display the result
     canvas.display_render().unwrap();
 }
